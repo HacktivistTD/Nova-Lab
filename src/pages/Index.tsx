fb92e25dff6d67e2b08f7eaadaf84db16e675d88
MenathNDGD <nadungmenath1@gmail.com>
2025-09-10 23:42:23 +0530

    refactor Index page to use Layout component and remove unnecessary div wrapper

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,21 @@
-import { Navbar } from '@/components/Navbar';
-
+import { Layout } from '@/layout/Layout';
 import { About } from '@/components/sections/About';
 import { Services } from '@/components/sections/Services';
 import { Portfolio } from '@/components/sections/Portfolio';
 import { Team } from '@/components/sections/Team';
 import { Contact } from '@/components/sections/Contact';
-import { Footer } from '@/components/Footer';
 import Hero from '@/components/sections/Hero';
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
+    <Layout>
       <Hero />
       <About />
       <Services />
       <Portfolio />
       <Team />
       <Contact />
-      <Footer />
-    </div>
+    </Layout>
   );
 };
 
